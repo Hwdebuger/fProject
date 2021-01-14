55ec465d95b27f5abec874a31da449ab32b32a5a
Alexey <hwdebuger@gmail.com>
2021-01-14 22:20:36 +0700

    14.01.21 -first state

---
+++ src/index.js
@@ -0,0 +1,10 @@
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello, React</h1>;
+  }
+}
+ReactDOM.render(
+    <Hello />,
+    document.getElementById("app")
+)
+console.log(32);
