f33a57e6b7a0c7ab277781dc9d8ca160cdc78147
Alexey <hwdebuger@gmail.com>
2021-01-16 01:15:35 +0700

    folder test

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+
 const hostname = 'localhost';
 const port = 3000;
 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ class Hello extends React.Component {
     return <h1>Hello, React</h1>;
   }
 }
-ReactDOM.render(
-    <Hello />,
-    document.getElementById("app")
-)
-let asd,dsa;
-console.log(asd);
-console.log(dsa);
+
+let personalMoveDB = { count: 0, movies: {}, acters: {}, gebres: [], private: false, points: 0 };
+
+let a = prompt("Какой фильм Вы смотрели недавно?", ""),
+    b = prompt("Во сколько баллов оцениваете?", ""),
+    c = prompt("Какой фильм Вы смотрели недавно?", ""),
+    d = prompt("Во сколько баллов оцениваете?", "");
+
+
+
+
+    personalMoveDB.movies[a] = b;
+    personalMoveDB.movies[c] = d;
+  //  ReactDOM.render(<p>Фильм  {a}</p>, document.getElementById('film'));
+  //  ReactDOM.render(<p>Оценка  {personalMoveDB.movies[a]}</p>, document.getElementById('points'));
+  //  ReactDOM.render(<p>Фильм  {d}</p>, document.getElementById('film'));
+  //  ReactDOM.render(<p>Оценка  {personalMoveDB.movies[d]}</p>, document.getElementById('points'));
+ALert(personalMoveDB);
