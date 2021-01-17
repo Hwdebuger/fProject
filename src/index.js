282ef9bdb3bd1d9178763f944d349f21aeb1cdb9
Alexey <hwdebuger@gmail.com>
2021-01-17 16:54:09 +0700

    Add massive info

--- src/index.js
+++ src/index.js
@@ -6,31 +6,10 @@ class Hello extends React.Component {
 }
 
 
-/* START получение названия, свойств количества ключей объекта */
-const personalMoveDB =
-{
-  count: 1,
-  movies: {asd: '2'},
-  acters: {},
-  gebres: [],
-  private: false,
-  points: 0,
-  printStr: function (params) { console.log(params) }
 
-};
-
-for(let key in personalMoveDB){
-  if(typeof(personalMoveDB[key])==='function')
-   console.log(`${key}() = ${typeof(personalMoveDB[key])}`);
-}
-
-
-//console.log(`количество ключей объекта: ${Object.keys(personalMoveDB).length}`); //количество ключей объекта 
-//console.log(`перечисление ключей объекта:  ${Object.keys(personalMoveDB)}`); //перечисление ключей объекта 
-/* END  получение названия и количества ключей объекта */
 
 //for (let key in personalMoveDB) {
- // if (typeof (personalMoveDB[key]) == 'function') console.log(key);
+// if (typeof (personalMoveDB[key]) == 'function') console.log(key);
 //}
 
 
@@ -73,3 +52,69 @@ switch (a) {
   case "": break;
   default: alert("Введите название фильма mk2");
 }*/
+
+
+
+/* START получение названия, свойств количества ключей объекта */
+const personalMoveDB =
+{
+  count: 1,
+  movies: { first: 'MK2', last: "mk2" },
+  acters: {},
+  gebres: [],
+  private: false,
+  points: 0,
+  printStr: function (params) { console.log(params) }
+
+};
+
+/* деструктуризация объекта
+const {first,last} = personalMoveDB.movies;
+console.log(first,last);
+*/
+
+
+/*
+for(let key in personalMoveDB){
+  if(typeof(personalMoveDB[key])==='function')
+   console.log(`${key}() = ${typeof(personalMoveDB[key])}`);
+}
+*/
+
+//console.log(`количество ключей объекта: ${Object.keys(personalMoveDB).length}`); //количество ключей объекта 
+//console.log(`перечисление ключей объекта:  ${Object.keys(personalMoveDB)}`); //перечисление ключей объекта 
+
+
+
+
+/* END  получение названия и количества ключей объекта */
+
+
+
+
+//-------------------------------------------------------------------
+// работа с массивами, начало
+//-------------------------------------------------------------------
+
+// пример сортировка массива как чисел*/  
+const arr = [2, 3, 4, 5, 11, 9];
+console.log(arr.sort(cNum));
+function cNum(a, b) {
+  return a - b;
+
+}
+
+// пример сортировка массива как строки*/
+const arr = ["asd", "zasd", "csd"];
+console.log(arr.sort());
+
+// пример перебора массива с помощью forEach()
+arr.forEach(function (item, i, arr) {
+  console.log(`${item} ${i} ${arr}`)
+
+});
+
+
+//-------------------------------------------------------------------
+//работа с массивами, конец 
+//-------------------------------------------------------------------
