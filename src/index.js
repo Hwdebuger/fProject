dc1e839b880a74d6183601bec680c6711ec4e5f5
Alexey <hwdebuger@gmail.com>
2021-01-21 23:04:29 +0700

    Add massive info

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-"use strict";
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello, React</h1>;
-  }
-}
+//'use strict';
+//class Hello extends React.Component {
+// render() {
+// return <h1>Hello, React</h1>;
+// }
+//}
 
-
-
-
+/*
 //for (let key in personalMoveDB) {
 // if (typeof (personalMoveDB[key]) == 'function') console.log(key);
 //}
 
 
 
-let a, b, c;
 
 function main() {
 
@@ -25,7 +22,7 @@ if (main()) prompt("Во сколько баллов оцениваете?", "")
 
 
 
-/*
+
 c = prompt("Сколько фильмов Вы смотрели недавно?", "");
 if (c) {
 
@@ -37,25 +34,26 @@ if (c) {
   }
 } else { alert("ответьте на вопрос"); }
 console.log(personalMoveDB);
-*/
+
 //personalMoveDB.movies[c] = d;
 
 //  ReactDOM.render(<p>Фильм  {a}</p>, document.getElementById('film'));
 //  ReactDOM.render(<p>Оценка  {personalMoveDB.movies[a]}</p>, document.getElementById('points'));
 //  ReactDOM.render(<p>Фильм  {d}</p>, document.getElementById('film'));
 //  ReactDOM.render(<p>Оценка  {personalMoveDB.movies[d]}</p>, document.getElementById('points'));
-/*if (a) console.log(personalMoveDB);
+if (a) console.log(personalMoveDB);
 
 switch (a) {
   case "mk2": console.log(a);
     break;
   case "": break;
   default: alert("Введите название фильма mk2");
-}*/
+}
 
 
 
-/* START получение названия, свойств количества ключей объекта */
+// START получение названия, свойств количества ключей объекта 
+
 const personalMoveDB =
 {
   count: 1,
@@ -68,18 +66,18 @@ const personalMoveDB =
 
 };
 
-/* деструктуризация объекта
+// деструктуризация объекта
 const {first,last} = personalMoveDB.movies;
 console.log(first,last);
-*/
 
 
-/*
+
+
 for(let key in personalMoveDB){
   if(typeof(personalMoveDB[key])==='function')
    console.log(`${key}() = ${typeof(personalMoveDB[key])}`);
 }
-*/
+
 
 //console.log(`количество ключей объекта: ${Object.keys(personalMoveDB).length}`); //количество ключей объекта 
 //console.log(`перечисление ключей объекта:  ${Object.keys(personalMoveDB)}`); //перечисление ключей объекта 
@@ -87,7 +85,7 @@ for(let key in personalMoveDB){
 
 
 
-/* END  получение названия и количества ключей объекта */
+// END  получение названия и количества ключей объекта 
 
 
 
@@ -96,7 +94,7 @@ for(let key in personalMoveDB){
 // работа с массивами, начало
 //-------------------------------------------------------------------
 
-// пример сортировка массива как чисел*/  
+// пример сортировка массива как чисел  
 const arr = [2, 3, 4, 5, 11, 9];
 console.log(arr.sort(cNum));
 function cNum(a, b) {
@@ -104,7 +102,7 @@ function cNum(a, b) {
 
 }
 
-// пример сортировка массива как строки*/
+// пример сортировка массива как строки
 const arr = ["asd", "zasd", "csd"];
 console.log(arr.sort());
 
@@ -161,3 +159,60 @@ console.log(minWind_1);
 //-------------------------------------------------------------------
 //работа с прототипами, конец 
 //-------------------------------------------------------------------
+
+*/
+//------ test personalMoveDb add, начало
+
+let a, b, c;
+let numberOfFilms;
+const personalMoveDB =
+{
+  count: 0,
+  movies: {
+    first: 'MK2',
+    last: "mk2"
+  },
+  acters: {},
+  gebres: [],
+  private: true,
+  points: 0,
+  printStr: function (params) { console.log(params) },
+
+  toggleVisibleMyDb: function () {
+    if (this.private == true)
+      personalMoveDB.private = false;
+    else
+      personalMoveDB.private = true;
+  },
+
+  start: function () {
+    let param = 0;
+    param = +prompt("Сколько фильмов Вы смотрели недавно?", "");
+    for (; param == "" || param == null || isNaN(param);) {
+      param = +prompt(`Сколько фильмов Вы смотрели недавно ?`, "");
+    }
+    personalMoveDB.count = param;
+    return param;
+  }
+
+};
+
+numberOfFilms = personalMoveDB.start();
+// недоделал 20.01.21
+for (let seen = false, c = numberOfFilms; c > 0; c--) {
+  a = prompt("Какой фильм Вы смотрели недавно?", "");
+  if (a != null && a != "" && a.length < 40) { //если строка введина правильно
+    b = prompt("Во сколько баллов оцениваете?", "");
+    personalMoveDB.movies[a] = b;
+  }
+ 
+  if (b != null && b != "") { //если строка введина правильно
+    personalMoveDB.movies[a] = b;
+    seen = true;
+  }
+  else {
+    c++;
+  }
+}
+
+//------ test personalMoveDb add, конец
